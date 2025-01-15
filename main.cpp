2a8ce6364c728848835ea2b94d9d4afe733e7180
BlueCannonBall <mmttiizz100@gmail.com>
2025-01-15 12:47:54 -0500

    Undo button change

--- main.cpp
+++ main.cpp
@@ -220,7 +220,6 @@ int main() {
                 dpp::component continue_button;
                 continue_button.set_type(dpp::cot_button);
                 continue_button.set_label("Keep looking");
-                continue_button.set_style(dpp::cos_secondary);
                 continue_button.set_emoji("🔍");
                 continue_button.set_id(json(
                     {
@@ -265,7 +264,6 @@ int main() {
                 dpp::component continue_button;
                 continue_button.set_type(dpp::cot_button);
                 continue_button.set_label("Keep looking");
-                continue_button.set_style(dpp::cos_secondary);
                 continue_button.set_emoji("🔍");
                 continue_button.set_id(json(
                     {
